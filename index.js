ecaef489c8e41519a251cbd83a2cf812b7eea2bb
Rofiq Ahmad Mubarok <rofiqahmadm@gmail.com>
2023-02-07 08:58:12 +0700

    create index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const { sqrt } = require("mathjs");
+console.log("sqrt and pow result:");
 
+// sqrt
+const { sqrt } = require("mathjs");
 console.log(sqrt(9).toString());
+
+// pow
+const { pow } = require("mathjs");
+console.log(pow(2, 2).toString());
